692f7f506424d8a4d714546f19b74d617549bb22
Klemen-Z <klemenzmahar@gmail.com>
2022-03-25 10:36:10 +0100

    added junit (bugged af) and move to projectiles

--- src/com/main/ptsd/Player_Projectile.java
+++ src/com/main/ptsd/Player_Projectile.java
@@ -6,4 +6,9 @@ public class Player_Projectile extends Projectile{
     Player_Projectile(int speed, int damage, int x, int y, int size) {
         super(speed, damage, x, y, size);
     }
+
+    public void move(){
+        int speed = 8;
+        setY(getY() - speed);
+    }
 }

a7a527ca0b96df1bb39b3302acc503ac0216e71e
Klemen-Z <klemenzmahar@gmail.com>
2022-04-04 11:10:22 +0200

    help

--- src/test/Java/com/main/ptsd/TestMain.java
+++ src/test/Java/com/main/ptsd/TestMain.java
@@ -1,6 +1,8 @@
 package com.main.ptsd;
 
 import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -13,13 +15,13 @@ public class TestMain {
         Scoreboard bob = new Scoreboard();
         Date date = new Date();
         bob.addEntry(10, date);
-        Thread.sleep(1500);
+        Thread.sleep(1000);
         date = new Date();
         bob.addEntry(20, date);
-        Thread.sleep(1500);
+        Thread.sleep(1000);
         date = new Date();
         bob.addEntry(100, date);
-        Thread.sleep(1500);
+        Thread.sleep(1000);
         date = new Date();
         bob.addEntry(50, date);
 
@@ -27,21 +29,25 @@ public class TestMain {
     }
     @SuppressWarnings("unchecked")
     public void dataStore(Date d, Integer wave, String fileName) throws IOException {
-        FileWriter file = new FileWriter(fileName);
         JSONObject json = new JSONObject();
+        JSONArray jsonA;
         if (checkFileContentExists(fileName)){
+            System.out.println("I append");
+            JSONParser jsonP = new JSONParser();
             json.put("date", "" + d + "");
             json.put("wave", wave);
-            try{
-                file.append(", ").append(json.toJSONString());
+            try (FileWriter file = new FileWriter(fileName); FileReader reader = new FileReader(fileName)){
+                jsonA = (JSONArray) jsonP.parse(reader);
+                jsonA.add(json);
+                file.write(jsonA.toJSONString());
                 file.flush();
-            } catch (IOException e) {
+            } catch (IOException | ParseException e) {
                 e.printStackTrace();
             }
         } else {
             json.put("date", "" + d + "");
             json.put("wave", wave);
-            try {
+            try (FileWriter file = new FileWriter(fileName)){
                 file.write(json.toJSONString());
                 file.flush();
             } catch (IOException e) {
@@ -50,12 +56,19 @@ public class TestMain {
         }
     }
     public boolean checkFileContentExists(String fileName) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        return br.readLine() != null;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            return br.readLine() != null;
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Test
-    public void jsonFileTest() throws IOException {
+    public void jsonFileTest() throws IOException, InterruptedException {
         dataStore(new Date(), 5, "Highscore.json");
+        Thread.sleep(1000);
+        dataStore(new Date(), 10, "Highscore.json");
     }
 }

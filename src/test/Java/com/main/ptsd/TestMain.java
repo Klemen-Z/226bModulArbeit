2532576b9b2d8f85363c0240c378811420bed8f0
Klemen-Z <klemenzmahar@gmail.com>
2022-04-04 08:30:20 +0200

    merge broke library json-simple. I added it again

--- src/test/Java/com/main/ptsd/TestMain.java
+++ src/test/Java/com/main/ptsd/TestMain.java
@@ -1,8 +1,6 @@
 package com.main.ptsd;
 
 import org.json.simple.*;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
